548d2d14998751e62b0ebf4e42e524df9f006026
venkateshsure <venky007sure@gmail.com>
2024-08-26 10:31:04 +0530

    testing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,39 +1,3 @@
-// import { Controller, Get, Post, Param, Body, Put, Delete, ParseIntPipe } from '@nestjs/common';
-// import { UserService } from './user.service';
-// import { User } from './user.entity';
-
-// @Controller('user')
-// export class UserController {
-//   constructor(private readonly userService: UserService) {}
-
-//   @Get()
-//   async findAll(): Promise<User[]> {
-//     return await this.userService.findAll();
-//   }
-
-//   @Get(':id')
-//   async findOne(@Param('id', ParseIntPipe) id: number): Promise<User> {
-//     return await this.userService.findOne(id);
-//   }
-
-//   @Post()
-//   async create(@Body() user: User): Promise<User> {
-//     return await this.userService.create(user);
-//   }
-
-//   @Delete(':id')
-//   async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
-//     await this.userService.remove(id);
-//   }
-
-//   @Put(':id')
-//   async update(
-//     @Param('id', ParseIntPipe) id: number,
-//     @Body('isActive') isActive: boolean,
-//   ): Promise<User | null> {
-//     return await this.userService.update(id, isActive);
-//   }
-// }
 import { Controller, Get, Post, Put, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
